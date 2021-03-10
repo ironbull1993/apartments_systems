cf2e8261dc870dac9175f7963d66de604453247f
ironbull1993 <mmbuji88@gmail.com>
2021-03-10 17:29:50 +0300

    new projects

---
+++ ams/config.php
@@ -0,0 +1,12 @@
+<?php
+define('_AMSCODESECURITY', '16343942');
+define('CURRENCY', '$');
+define('WEB_URL', 'http://localhost/ams/');
+define('ROOT_PATH', 'C:\wamp64\www\ams/');
+
+
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'adjmtwad');
+define('DB_DATABASE', 'ams');
+$link = new mysqli(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD,DB_DATABASE);?>
